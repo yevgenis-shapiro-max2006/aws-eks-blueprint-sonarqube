fbf140a45e004e77c137c956d3e000215c7766b1
apps <23049337+yevgenis-shapiro-max2006@users.noreply.github.com>
2026-09-12 18:35:50 +0300

    update
    Removed unused Blockscout variables from the configuration.

--- variables.tf
+++ variables.tf
@@ -51,25 +51,3 @@ variable "make_gp3_default" {
   default = true
 }
 
-variable "blockscout_postgresql_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "blockscout_rpc_url" {
-  type      = string
-  sensitive = true
-}
-
-variable "blockscout_network" {
-  type = string
-}
-
-variable "blockscout_subnetwork" {
-  type    = string
-  default = ""
-}
-
-variable "blockscout_hostname" {
-  type = string
-}
